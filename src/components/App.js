45e71dd839713fd1523ffc983ca93e0f3b0bfba4
colinskorir <korircoll20@gmail.com>
2025-04-28 09:09:45 +0300

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import posts from "../data/posts";
 
-function App({ name, image, about, posts }) {
+function App() {
   return (
     <div className="App">
-      <Header name={name} />
-      <About image={image} about={about} />
-      <main>
-        <ArticleList posts={posts} />
-      </main>
+      <Header name="Underreacted" />
+      <About
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7q0rY4krTnCmx0PCVrtX3TQzQzgBXfNflng&s"
+        about="A blog about React"
+      />
+      <ArticleList posts={posts} />
     </div>
   );
 }
